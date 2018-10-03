fce8b7b58ebe15a2f4fc9ba727bdf8d309d3e689
Shlomi Noach <shlomi-noach@github.com>
2018-10-03 13:30:32 +0300

    copyright

--- go/cmd/vtgate/vtgate.go
+++ go/cmd/vtgate/vtgate.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 Google Inc.
+Copyright 2018 GitHub Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -90,6 +90,5 @@ func main() {
 		discovery.ParseTabletURLTemplateFromFlag()
 		addStatusParts(vtg)
 	})
-
 	servenv.RunDefault()
 }
